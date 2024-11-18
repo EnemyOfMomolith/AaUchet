d080efa91c5dfb9787cacd6e6cf882dc3101d6a3
Awareness <Awareness@HOME-PC>
2024-11-19 01:06:59 +0500

    Добавьте файлы проекта.

---
+++ AaUchet/AddEditPage.xaml.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AaUchet
+{
+    /// <summary>
+    /// Логика взаимодействия для AddEditPage.xaml
+    /// </summary>
+    public partial class AddEditPage : Page
+    {
+        private Преподаватели _currentPrepod = new Преподаватели();
+        public AddEditPage(Преподаватели selectedPrepod)
+        {
+            InitializeComponent();
+            if (selectedPrepod != null)
+            {
+                _currentPrepod = selectedPrepod;
+                ComboType.SelectedIndex = _currentPrepod.КодКафедры - 2;
+            }
+
+            DataContext = _currentPrepod;
+        }
+
+        private void UploadPhotoButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog myOpenFileDialog = new OpenFileDialog();
+            if (myOpenFileDialog.ShowDialog() == true)
+            {
+
+                FileInfo fileInfo = new FileInfo(myOpenFileDialog.FileName);
+
+
+                if (fileInfo.Length < 2 * 1024 * 1024)
+                {
+                    _currentPrepod.Фото = myOpenFileDialog.FileName;
+                    _photo.Source = new BitmapImage(new Uri(myOpenFileDialog.FileName));
+                }
+                else
+                {
+
+                    MessageBox.Show("Размер файла превышает 2 мегабайта. Выберите другой файл.");
+                }
+            }
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder err = new StringBuilder();
+
+            if (string.IsNullOrWhiteSpace(_currentPrepod.Имя))
+                err.AppendLine("Укажите имя");
+            if (string.IsNullOrWhiteSpace(_currentPrepod.Фамилия))
+                err.AppendLine("Укажите фамилию");
+            if (string.IsNullOrWhiteSpace(_currentPrepod.Отчество))
+                err.AppendLine("Укажите отчество");
+            if (string.IsNullOrWhiteSpace(_currentPrepod.Должность))
+                err.AppendLine("Укажите должность");
+                if(DateTime.TryParse(tb_experience.Text, out DateTime newDate))
+                {
+                    _currentPrepod.ГодПоступления = newDate;
+                }
+                else{
+                    err.AppendLine("Укажите дату выхода на работу");
+                }
+            if (string.IsNullOrWhiteSpace(_currentPrepod.Адрес))
+                err.AppendLine("Укажите адрес");
+            if (GenderComboBox.SelectedItem == null)
+            {
+                MessageBox.Show("Пожалуйста, выберите пол.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+            if (ComboType.SelectedItem == null)
+                err.AppendLine("Укажите Кафедру ");
+            else
+            {
+                _currentPrepod.КодКафедры = ComboType.SelectedIndex + 2;
+            }
+
+            if (err.Length > 0)
+            {
+                MessageBox.Show(err.ToString());
+                return;
+            }
+
+            if (_currentPrepod.КодПреподавателя == 0)
+                _currentPrepod.КодПреподавателя = UchetUSPEntities.GetContext().Преподаватели.Select(p=>p.КодПреподавателя).Max()+1;
+                UchetUSPEntities.GetContext().Преподаватели.Add(_currentPrepod);
+
+            try
+            {
+                _currentPrepod.Пол = GenderComboBox.Text;
+                _currentPrepod.Телефон = "7(937)964-02-58";
+                _currentPrepod.ГодРождения = DateTime.Parse("12.12.2000");
+                _currentPrepod.Город = "test";
+                UchetUSPEntities.GetContext().SaveChanges();
+                MessageBox.Show("информация сохранена");
+                manager.MainFrame.GoBack();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
---
+++ AaUchet/prepodPage.xaml.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AaUchet
+{
+    /// <summary>
+    /// Логика взаимодействия для prepodPage.xaml
+    /// </summary>
+    public partial class prepodPage : Page
+    {
+        public prepodPage()
+        {
+            InitializeComponent();
+
+            var currentPrepod = UchetUSPEntities.GetContext().Преподаватели.ToList();
+            PrepodListView.ItemsSource = currentPrepod;
+
+
+            DepartmentComboBox.SelectedIndex = 0;
+
+            Update();
+            PrepodListView.Items.Refresh();
+        }
+        private void Update()
+        {
+            var currentPrepod = UchetUSPEntities.GetContext().Преподаватели.ToList();
+
+            var start_count = currentPrepod.Count;
+
+            if (DepartmentComboBox.SelectedIndex == 0)
+            {
+                currentPrepod = currentPrepod;
+            }
+            if (DepartmentComboBox.SelectedIndex == 1)
+            {
+                currentPrepod = currentPrepod.Where(p => (p.КодКафедры == 2)).ToList();
+            }
+            if (DepartmentComboBox.SelectedIndex == 2)
+            {
+                currentPrepod = currentPrepod.Where(p => (p.КодКафедры == 3)).ToList();
+            }
+            if (DepartmentComboBox.SelectedIndex == 3)
+            {
+                currentPrepod = currentPrepod.Where(p => (p.КодКафедры == 4)).ToList();
+            }
+            if (DepartmentComboBox.SelectedIndex == 4)
+            {
+                currentPrepod = currentPrepod.Where(p => (p.КодКафедры == 5)).ToList();
+            }
+            if (DepartmentComboBox.SelectedIndex == 5)
+            {
+                currentPrepod = currentPrepod.Where(p => (p.КодКафедры == 6)).ToList();
+            }
+
+            currentPrepod = currentPrepod.Where(p => p.Имя.ToLower().Contains(SearchBox.Text.ToLower()) ||
+            p.Фамилия.ToLower().Contains(SearchBox.Text.ToLower()) ||
+            p.Отчество.ToLower().Contains(SearchBox.Text.ToLower())).ToList();
+
+            if (SortAscRadioButton.IsChecked.Value)
+            {
+                currentPrepod = currentPrepod.OrderBy(p => p.Стаж).ToList();
+
+            }
+            if (SortDescRadioButton.IsChecked.Value)
+            {
+                currentPrepod = currentPrepod.OrderByDescending(p => p.Стаж).ToList();
+            }
+
+            TBCount.Text = string.Format("показано преподавателей {0} из {1}", currentPrepod.Count, start_count);
+
+            PrepodListView.ItemsSource = currentPrepod;
+
+
+
+
+        }
+
+        private void AddPrepod_Click(object sender, RoutedEventArgs e)
+        {
+            manager.MainFrame.Navigate(new AddEditPage(null));
+            Update();
+            PrepodListView.Items.Refresh();
+        }
+
+        private void editPrepod_Click(object sender, RoutedEventArgs e)
+        {
+            manager.MainFrame.Navigate(new AddEditPage((sender as Button).DataContext as Преподаватели));
+            Update();
+            PrepodListView.Items.Refresh();
+        }
+
+        private void deletePrepod_Click(object sender, RoutedEventArgs e)
+        {
+            var currentPrepod = (sender as Button).DataContext as Преподаватели;
+
+            if (currentPrepod.Стаж < 1)
+            {
+                MessageBox.Show("Нельзя удалить преподавателя со стажем меньше 1 года.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+
+            if (MessageBox.Show("Вы точно хотите удалить?", "Внимание!", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                    UchetUSPEntities.GetContext().Преподаватели.Remove(currentPrepod);
+                    UchetUSPEntities.GetContext().SaveChanges();
+                    PrepodListView.ItemsSource = UchetUSPEntities.GetContext().Преподаватели.ToList();
+                    Update();
+            }
+
+         
+        }
+
+        private void SearchBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Update();
+            PrepodListView.Items.Refresh();
+
+        }
+
+        private void DepartmentComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Update();
+            PrepodListView.Items.Refresh();
+
+        }
+
+        private void SortAscRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            Update();
+            PrepodListView.Items.Refresh();
+
+        }
+
+        private void SortDescRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            Update();
+            PrepodListView.Items.Refresh();
+
+        }
+
+        private void PrepodListView_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            Update();
+
+        }
+
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (Visibility == Visibility.Visible)
+            {
+                UchetUSPEntities.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+                PrepodListView.ItemsSource = UchetUSPEntities.GetContext().Преподаватели.ToList();
+                Update();
+            }
+        }
+
+        private void PrepodListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Update();
+            PrepodListView.Items.Refresh();
+        }
+    }
+}
